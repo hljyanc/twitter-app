967c83ddd815328cd6059fa22695ff49c302c0f5
hljyanc <hljyanc@gmail.com>
2023-07-19 23:10:17 -0700

    feat: 🎸 add eslint config

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,33 +1,37 @@
-import { useState } from 'react';
-import { Button, Dialog, Form, Input, Dialog } from 'antd-mobile';
+import {
+  Button, Dialog, Form, Input,
+} from 'antd-mobile';
 import './index.css';
 
-function Login() {
-    const [form] = Form.useForm();
-    const onSubmit = () => {
-        const values = form.getFieldsValue();
-        Dialog.alert({
-            content: JSON.stringify(values),
-        })
-    }
+const Login = () => {
+  const [form] = Form.useForm();
+  const onSubmit = () => {
+    const values = form.getFieldsValue();
+    Dialog.alert({
+      content: JSON.stringify(values),
+    });
+  };
   return (
     <div className="login">
-      <Form layout="horizontal" mode="card" from={form}
-        footer={
-            <Button block color='primary' onClick={onSubmit} size='large'>
-                登录
-            </Button>
-        }
+      <Form
+        layout="horizontal"
+        mode="card"
+        from={form}
+        footer={(
+          <Button block color="primary" onClick={onSubmit} size="large">
+            登录
+          </Button>
+          )}
       >
         <Form.Item label="Username" name="username">
-          <Input placeholder="please enter username" clearable/>
+          <Input placeholder="please enter username" clearable />
         </Form.Item>
         <Form.Item label="Password" name="password">
-          <Input placeholder="please enter password" type='password' clearable/>
+          <Input placeholder="please enter password" type="password" clearable />
         </Form.Item>
       </Form>
     </div>
   );
-}
+};
 
 export default Login;
